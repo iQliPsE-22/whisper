d64efbfc0ec2461b9aaefaaf790cff25c1b4b023
Deepak Kumar <deepaklg02@gmail.com>
2024-03-04 00:07:05 +0530

    mm

---
+++ app/components/Chat.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import Image from "next/image";
+
+const Chat = (props) => {
+  const [isImageClicked, setIsImageClicked] = useState(false);
+
+  const handleImageClick = () => {
+    setIsImageClicked(!isImageClicked);
+  };
+
+  return (
+    <div className="h-20 bg-[#282828] flex items-center mb-0.75">
+      <div id={isImageClicked} className="ml-4">
+        <Image
+          src={props.imgSrc}
+          alt="dp"
+          width={isImageClicked ? 200 : 60}
+          height={isImageClicked ? 200 : 60}
+          className="rounded-full"
+          quality={100}
+          priority={true}
+          onClick={handleImageClick}
+        />
+      </div>
+      <h3 className="ml-4">{props.userName}</h3>
+    </div>
+  );
+};
+
+export default Chat;
--- app/home/page.js
+++ app/home/page.js
@@ -1,15 +1,28 @@
+"use client";
 import React from "react";
 import Header from "./../components/Header";
+import Chat from "./../components/Chat";
+import Image from "next/image";
 
 const friend = {
   name: "John",
   email: "john@gmail.com",
+  dp: "/pic2.png",
+};
+
+const friend2 = {
+  name: "Jane",
+  email: "jane@gmail.com",
+  dp: "/pic.jpg",
 };
 
 const page = () => {
   return (
     <>
-      <div>page</div>
+      <div>
+        <Chat imgSrc={friend.dp} userName={friend.name} />
+        <Chat imgSrc={friend2.dp} userName={friend2.name} />
+      </div>
     </>
   );
 };
