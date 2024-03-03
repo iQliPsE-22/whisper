2460594b61919b3dc103fd09bca71d275f4d07f1
Deepak Kumar <deepaklg02@gmail.com>
2024-03-03 14:08:10 +0530

    Dynamic

--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
-import logo from "./Assets/whisper.svg";
 import Image from "next/image";
-
+import dynamic from "next/dynamic";
+const logo = dynamic(() => import("./Assets/whisper.svg"));
 const page = () => {
   return (
     <div className="h-screen w-screen text-center flex items-center justify-center">
       <div className="logo">
-        <Image priority src={logo} alt = "logo"/>
+        <Image priority src={logo} alt="logo" />
       </div>
     </div>
   );
--- app/signup/page.js
+++ app/signup/page.js
@@ -1,6 +1,5 @@
 "use client";
 import React, { useState } from "react";
-import Image from "next/image";
 import "../login/login.css";
 import Link from "next/link";
 import dynamic from "next/dynamic";
