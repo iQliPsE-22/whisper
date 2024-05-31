92e790782f8db844eb7d38448c977b16ca19f387
Deepak Kumar <deepaklg02@gmail.com>
2024-05-31 22:52:36 +0530

    login link

--- app/login/page.js
+++ app/login/page.js
@@ -16,14 +16,11 @@ const Page = () => {
   const [error, setError] = useState("");
 
   const { userData, setUserData } = useUser();
-
-  const fetchUser = async (e) => {};
-
   const handleFormSubmit = async (e) => {
     e.preventDefault();
     try {
       console.log("Login Attempted");
-      const response = await fetch("http://localhost:3000/login", {
+      const response = await fetch("https://server-hush.vercel.app/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
