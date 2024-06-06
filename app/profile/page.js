8d9322347aef5bc85497b5ae5ddb9d508e4aae7e
Deepak Kumar <deepaklg02@gmail.com>
2024-06-06 19:35:16 +0530

    server linl

--- app/profile/page.js
+++ app/profile/page.js
@@ -56,8 +56,7 @@ const Page = () => {
   const handleDeleteAccount = async () => {
     try {
       const response = await fetch(
-        `https://server-hush.vercel.app
-        /user/${userData.user._id}`,
+        `https://server-hush.vercel.app/user/${userData.user._id}`,
         {
           method: "DELETE",
         }
