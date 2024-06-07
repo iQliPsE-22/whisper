f5b6459c58099a4690f849164f1aa3839bc0e562
Deepak Kumar <deepaklg02@gmail.com>
2024-06-07 12:20:40 +0530

    login opening

--- app/login/page.js
+++ app/login/page.js
@@ -1,5 +1,5 @@
 "use client";
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import Image from "next/image";
 import dynamic from "next/dynamic";
 import Link from "next/link";
