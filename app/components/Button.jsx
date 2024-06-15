611c69161ce0bfccdab2e8ea1d3321d1c3518e9a
Deepak Kumar <deepaklg02@gmail.com>
2024-06-15 09:02:19 +0530

    chat

--- app/components/Button.jsx
+++ app/components/Button.jsx
@@ -1,6 +1,6 @@
 const Button = (props) => {
   return (
-    <div className="flex justify-center w-full bgColor-red z-1">
+    <div className=" julius flex justify-center w-full bgColor-red z-1">
       <button
         type={props.type}
         className={`w-dvw lg:w-3/12 p-3 lg:p-4 mb-2 text-black rounded-lg flex justify-center items-center cursor-pointer z-2`}
--- app/components/ChatBox.jsx
+++ app/components/ChatBox.jsx
@@ -1,20 +1,16 @@
 import React from "react";
-
+import "./chatBox.css";
 const ChatBox = ({ msg, sender }) => {
   const isEmoji = msg.message.match(/\p{Emoji}/gu);
 
   return (
     <div className="text-white w-full m-2 flex flex-col items-start">
       <div
-        className={`w-fit block min-h-fit bg-[#1A1A1A] m-2 border-2 p-0 ${
+        className={`text-box break-words w-fit block min-h-fit bg-[#1A1A1A] m-2 border-2 p-0 ${
           msg.sender === sender
             ? "border-rose-500 self-end rounded-l-lg rounded-b-lg rounded-br-2xl"
             : "border-green-500 self-start rounded-e-2xl rounded-l rounded-bl-3xl"
         }`}
-        style={{
-          maxWidth: "50%", // Adjusted to 60%
-          wordWrap: "break-word", // Add word-wrap property
-        }}
       >
         <h1 className={`p-4 text-center font-xl ${isEmoji ? "text-4xl" : ""}`}>
           {msg.message}
